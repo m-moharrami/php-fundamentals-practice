17645c66f5fb80de283a2a5d07ad1b2012504eb2
m-moharrami <moharram.m98@gmail.com>
2023-04-08 13:52:13 +0330

    cookies done

---
+++ 12_cookies.php
@@ -0,0 +1,19 @@
+<?php
+/* ---------------- Cookies ---------------- */
+
+/*
+    Cookies are a mechanism for storing data in the remote browser
+    and thus tracking or identifying return users. You can set
+    specific data to be stored in the browser, and then retrieve
+    it when the user visits the site again.
+*/
+
+// Set cookie
+setcookie('name', 'Moharram', time() + 86400);
+
+if(isset($_COOKIE['name'])) {
+    echo $_COOKIE['name'];
+}
+
+// Delete cookie 
+setcookie('name', '', time() - 86400);
