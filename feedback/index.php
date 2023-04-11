c895217a424c99d069f5702fe5b02f97fb999a45
m-moharrami <moharram.m98@gmail.com>
2023-04-11 11:52:43 +0330

    add styling when there is input error

--- feedback/index.php
+++ feedback/index.php
@@ -46,30 +46,39 @@
             <label for="name" class="form-label">Name</label>
             <input
                 type="text"
-                class="form-control"
+                class="form-control <?php echo $nameErr ? 'is-invalid' : null; ?>"
                 id="name"
                 name="name"
                 placeholder="Enter your name"
             />
+            <div class="invalid-feedback">
+                <?php echo $nameErr; ?>
+            </div>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input
                 type="email"
-                class="form-control"
+                class="form-control <?php echo $emailErr ? 'is-invalid' : null; ?>"
                 id="email"
                 name="email"
                 placeholder="Enter your email"
             />
+            <div class="invalid-feedback">
+                <?php echo $emailErr; ?>
+            </div>
         </div>
         <div class="mb-3">
             <label for="body" class="form-label">Feedback</label>
             <textarea
-                class="form-control"
+                class="form-control <?php echo $bodyErr ? 'is-invalid' : null; ?>"
                 id="body"
                 name="body"
                 placeholder="Enter your feedback"
             ></textarea>
+            <div class="invalid-feedback">
+                <?php echo $bodyErr; ?>
+            </div>
         </div>
         <div class="mb-3">
             <input
