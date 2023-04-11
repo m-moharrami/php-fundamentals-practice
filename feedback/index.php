2d653b1aa9d7fed2aee9b96540e8ec8f477d779e
m-moharrami <moharram.m98@gmail.com>
2023-04-11 11:43:19 +0330

    validate input fields

--- feedback/index.php
+++ feedback/index.php
@@ -1,4 +1,37 @@
 <?php include 'inc/header.php'; ?>
+
+<?php 
+    $name = $email = $body = '';
+    $nameErr = $emailErr = $bodyErr = '';
+
+    // Form submit 
+    if(isset($_POST['submit'])) {
+        // Validate name
+        if(empty($_POST['name'])) {
+            $nameErr = 'Name is required';
+        } else {
+            $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        }
+
+        // Validate email
+        if(empty($_POST['email'])) {
+            $emailErr = 'Email is required';
+        } else {
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        }
+
+        // Validate body
+        if(empty($_POST['body'])) {
+            $bodyErr = 'Feedback is required';
+        } else {
+            $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        }
+    }
+
+    // echo $nameErr;
+    // echo $name;
+?>
+
     <img
         src="/php-fundamentals-practice/feedback/img/logo.png"
         class="w-25 mb-3"
@@ -8,7 +41,7 @@
     <p class="lead text-center">
         Leave feedback for Traversy Media
     </p>
-    <form action="" class="mt-4 w-75">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="mt-4 w-75">
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input
